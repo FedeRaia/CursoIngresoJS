c103d9bdefaaca9ff6fbd54e9561f8a635c933e3
FedeRaia <f4r4w4y.exe@gmail.com>
2019-02-08 19:29:16 -0300

    saliò el 6 con ayuda

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -5,6 +5,7 @@ function sumar()
 {
     var num1=document.getElementById("numeroUno").value;
     var num2=document.getElementById("numeroDos").value;
-    
+    var a=parseInt(num1);
+    var b=parseInt(num2);
+    alert(a+b);
 }
-
