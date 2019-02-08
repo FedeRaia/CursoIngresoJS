9fe26e9f6ae304c22664a85861148266bd28de68
FedeRaia <f4r4w4y.exe@gmail.com>
2019-02-08 19:21:43 -0300

    comienzo de ejercicio 6

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,8 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+    var num1=document.getElementById("numeroUno").value;
+    var num2=document.getElementById("numeroDos").value;
+    
 }
 
