6e2ea9220de8b8cbf520b73c0ebc9f6dfba84b7c
FedeRaia <f4r4w4y.exe@gmail.com>
2019-02-08 19:52:14 -0300

    cuesta el 8

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,8 +3,8 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-    var num1=document.getElementById("numeroUno");
-    var num2=document.getElementById("numeroDos");
+    var num1=document.getElementById("numeroUno").value;
+    var num2=document.getElementById("numeroDos").value;
     var a=parseInt(num1);
     var b=parseInt(num2);
     alert(a+b);
