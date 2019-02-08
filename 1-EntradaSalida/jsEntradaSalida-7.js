e2001d22f4e3411613148877ba898ae58feb6b72
FedeRaia <f4r4w4y.exe@gmail.com>
2019-02-08 19:39:08 -0300

    el 7 soloooo

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,37 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+    var num1=document.getElementById("numeroUno").value
+    var num2=document.getElementById("numeroDos").value
+    var a=parseInt(num1);
+    var b=parseInt(num2);
+    alert(a+b)
 }
 
 function restar()
 {
-	
+    var num1=document.getElementById("numeroUno").value
+    var num2=document.getElementById("numeroDos").value
+    var a=parseInt(num1);
+    var b=parseInt(num2);
+    alert(a-b);
 }
 
 function multiplicar()
 { 
-	
+    var num1=document.getElementById("numeroUno").value
+    var num2=document.getElementById("numeroDos").value
+    var a=parseInt(num1);
+    var b=parseInt(num2);
+    alert(a*b);
 }
 
 function dividir()
 {
-	
+    var num1=document.getElementById("numeroUno").value
+    var num2=document.getElementById("numeroDos").value
+    var a=parseInt(num1);
+    var b=parseInt(num2);
+    alert(a/b);
 }
 
