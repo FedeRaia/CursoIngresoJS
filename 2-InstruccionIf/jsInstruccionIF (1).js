c6bd1a9f6708d73f80297da2c7b6a2ea4ec95089
FedeRaia <f4r4w4y.exe@gmail.com>
2019-02-08 21:14:35 -0300

    salio if 1

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
+var laEdad=document.getElementById("edad").value;
+var a=parseInt(laEdad);
 
-
+if (a==15)
+    {
+        alert("Niña Bonita");
+    }
+else
+    {
+        alert(a);
+    }
 
 }//FIN DE LA FUNCIÓN
