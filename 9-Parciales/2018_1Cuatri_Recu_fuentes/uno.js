6c873dd8118f779fbab3becae923fccf548312de
FedeRaia <f4r4w4y.exe@gmail.com>
2019-02-08 22:15:38 -0300

    recuperatorio

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,13 @@
 
 function mostrar()
 {
+    var dato1=prompt("Ingrese ancho del rectàngulo");
+    var dato2=prompt("Ingrese largo del rectàngulo");
 
+    var a=parseInt(dato1);
+    var b=parseInt(dato2);
+
+    var c=((a*2)+(b*2));
+
+    alert("el perìmetro del rectàngulo es "+c);
 }
