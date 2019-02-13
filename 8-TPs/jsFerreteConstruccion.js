9c85649ab6f3fe49c98e8803917b277329d45171
federaia <f4r4w4y.exe@gmail.com>
2019-02-13 20:45:48 -0300

    albañileria

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,32 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+var largo=document.getElementById("Largo").value;
+var ancho=document.getElementById("Ancho").value;
 
+var lar=parseInt(largo);
+var anc=parseInt(ancho);
+
+cantAlambre=(((lar*2)+(anc*2))*3);
+alert("La cantidad de alambre es: "+cantAlambre);
 }
 function Circulo () 
 {
-	
+var radio=document.getElementById("Radio").value;
+var circ=parseInt(radio);
+
+var alambreCirc=(3,14*(circ*circ));
+alert("Cantidad de alambre para circular: "+alambreCirc);
 }
 function Materiales () 
 {
-	
+var largo=document.getElementById("Largo").value;
+var ancho=document.getElementById("Ancho").value;
+    
+var lar=parseInt(largo);
+var anc=parseInt(ancho);
+
+var volumen=(lar*anc);
+alert("Cantidad de bolsas de cemento: "+volumen*2);
+alert("Cantidad de bolsas de cal: "+volumen*3);
 }
