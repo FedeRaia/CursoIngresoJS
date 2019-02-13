513cc49e85abbe81763a13df30c4fd337b780baa
FedeRaia <f4rf4w4y.exe@gmail.com>
2019-02-13 11:39:54 -0300

    hasta (if) en casa

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,7 @@
 function mostrar()
 {
-  
+var a=document.getElementById("elNombre").value;
+var b=document.getElementById("laLocalidad").value;
+
+alert("Usted es "+ a +" y vive en la localidad de "+b);
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,13 @@
 function mostrar()
 {
+var a=prompt("Ingrese precio.");
+var b=prompt("Ingrese descuento.");
 
+var precio=parseInt(a);
+var descuento=parseInt(b);
+
+var cálculo=(a/100*b);
+;var final=(a-cálculo);
+
+document.getElementById("elPrecioFinal").value=final;
 }
