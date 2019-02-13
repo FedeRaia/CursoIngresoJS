054d04006f8e283ed6d446c6cc5d346f54b76fbe
federaia <f4r4w4y.exe@gmail.com>
2019-02-13 20:12:43 -0300

    malditas operaciones

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,35 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    var prec1=document.getElementById("PrecioUno").value;
+    var prec2=document.getElementById("PrecioDos").value;
+    var prec3=document.getElementById("PrecioTres").value;
+    p1=parseInt(prec1);
+    p2=parseInt(prec2);
+    p3=parseInt(prec3);
+    suma=(p1+p2+p3);
+    alert("El total de la suma es: "+suma);
 }
 function Promedio () 
 {
-	
+    var prec1=document.getElementById("PrecioUno").value;
+    var prec2=document.getElementById("PrecioDos").value;
+    var prec3=document.getElementById("PrecioTres").value;
+    p1=parseInt(prec1);
+    p2=parseInt(prec2);
+    p3=parseInt(prec3);
+    promedio=((p1+p2+p3)/3);
+	alert("El promedio es: "+promedio);
 }
 function PrecioFinal () 
 {
-	
+    var prec1=document.getElementById("PrecioUno").value;
+    var prec2=document.getElementById("PrecioDos").value;
+    var prec3=document.getElementById("PrecioTres").value;
+    p1=parseInt(prec1);
+    p2=parseInt(prec2);
+    p3=parseInt(prec3);
+    iva=((p1+p2+p3)/100*21);
+    finaliva=(iva+(p1+p2+p3));
+	alert("La suma mas el IVA es: "+finaliva);
 }
