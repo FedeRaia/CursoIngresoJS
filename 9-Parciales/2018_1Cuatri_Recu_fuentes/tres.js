b654b34386553038bb831b47159a40b28b4b243b
federaia <f4r4w4y.exe@gmail.com>
2019-02-15 19:24:41 -0300

    ya

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -7,7 +7,7 @@ var precio=parseInt(a);
 var descuento=parseInt(b);
 
 var cálculo=(a/100*b);
-;var final=(a-cálculo);
+var final=(a-cálculo);
 
 document.getElementById("elPrecioFinal").value=final;
 }
