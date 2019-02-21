e05f4d31eed7157866536761cf56d52a658be2a5
alumno <alumno>
2019-02-20 22:14:56 -0300

    uile2
    Signed-off-by: alumno <alumno>

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -3,5 +3,10 @@ function mostrar()
 
 var clave = prompt("ingrese el número clave.");
 
+    while (clave!="123")
+        {
+            prompt("Clave incorrecta, ingrese nuevamente", "Aqui");
+        }
+
 
 }//FIN DE LA FUNCIÓN
