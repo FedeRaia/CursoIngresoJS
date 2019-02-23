cf5b70289e7b18caf53b5c8a0acbfbe2e3fb2ec9
federaia <f4r4w4y.exe@gmail.com>
2019-02-22 22:12:54 -0300

    uile 10
    Signed-off-by: federaia <f4r4w4y.exe@gmail.com>

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,47 @@
 function mostrar()
 {
 
-	var contador=0;
+	var contador=0, sumaNeg=0, sumaPos=0, cantPos=0, cantNeg=0, cantCeros=0, cantPar=0, promPos, proNeg, dif;
 	//declarar contadores y variables 
 	
-	var respuesta="si";
+	var respuesta=true;
 
-	while(respuesta!="no")
-	{
-		
-	
-	}
+	while(respuesta==true)
+		{
+			do
+			{
+				num=prompt("Ingrese numeros.");
+				num=parseInt(num);
+				respuesta=confirm("Continuar?");
+				contador++
+			}
+		while(isNaN(num))
+			{
+				if(num<0)
+					{
+						sumaNeg=(num+=num);
+						cantNeg++;
+					}
+				if(num>0)
+					{
+						sumaPos=(num+=num);
+						cantPos++;
+					}
+				if(num==0)
+					{
+						cantCeros++;
+					}
+				if(num%2==0)
+					{
+						cantPar++;
+					}
+				
+			}
+		}
 
-
+alert("La suma de negativos es "+sumaNeg+//
+" ,la suma de positivos es "+sumaPos+//
+" la cantidad de positivos "+cantPos+" la cantidad de negativos es "+cantNeg);
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,17 +2,43 @@ function mostrar()
 {
 
 	var contador=0;
+	var max;
+	var min;
+	var bandera=true;
+	var respuesta=true;
 	// declarar variables
-	
-	var respuesta='si';
 
-	while(respuesta!='no')
-	{
-		
-	
-	}
+	while(respuesta==true)
+		{
+			do
+				{
+					num=prompt("Ingrese numero.");
+					num=parseInt(num);
+					respuesta=confirm("Continuar?");
+					contador++;
+				}
+			while(isNaN(num))
+				{
+					if (bandera)
+						{
+							bandera=false;
+							max=num;
+							min=num;
+						}
+					else
+						{
+							if(num>max)
+								{
+									max=num;
+								}
+							if (num<min)
+								{
+									min=num;
+								}
+						}
+				}
+		}
 
-
-
-
+document.getElementById("maximo").value=max;
+document.getElementById("minimo").value=min;
 }//FIN DE LA FUNCIÓN
