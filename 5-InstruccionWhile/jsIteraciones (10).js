38ceea0c869e41c762ea41e60091911d1fa15b79
FedeRaia <f4rf4w4y.exe@gmail.com>
2019-02-24 20:56:24 -0300

    uile 10 errores

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,7 +1,7 @@
 function mostrar()
 {
 
-	var contador=0, sumaNeg=0, sumaPos=0, cantPos=0, cantNeg=0, cantCeros=0, cantPar=0, promPos, proNeg, dif;
+	var contador=0, sumaPos=0, sumaNeg=0, cantPos=0, cantNeg=0, cantCeros=0, cantPar=0, promPos, promNeg, dif;
 	//declarar contadores y variables 
 	
 	var respuesta=true;
@@ -15,6 +15,7 @@ function mostrar()
 				respuesta=confirm("Continuar?");
 				contador++
 			}
+		
 		while(isNaN(num))
 			{
 				if(num<0)
@@ -35,13 +36,26 @@ function mostrar()
 					{
 						cantPar++;
 					}
-				
-			}
+				if(respuesta==false)
+				{
+					
+				}	
+			promNeg=(cantNeg/contador);
+			promPos=(cantPos/contador);
+			dif=(sumaPos+sumaNeg);
+			}	
 		}
 
-alert("La suma de negativos es "+sumaNeg+//
+	
+	document.write("La suma de negativos es "+sumaNeg+//
 " ,la suma de positivos es "+sumaPos+//
-" la cantidad de positivos "+cantPos+" la cantidad de negativos es "+cantNeg);
+" la cantidad de positivos es "+cantPos+//
+" la cantidad de negativos es "+cantNeg+//
+" la cantidad de ceros es "+cantCeros+//
+" la cantidad de numeros pares es "+cantPar+//
+" el promedio de positivos es "+promPos+//
+" el promedio de negativos es "+promNeg+//
+" la diferencia de positivos y negativos es "+dif);
 
 
 }//FIN DE LA FUNCIÓN
